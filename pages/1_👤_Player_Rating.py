8745130930cc88a5cc1467f4d75ffc10a69764c8
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-08-16 21:20:08 +0200

    shap

--- pages/1_👤_Player_Rating.py
+++ pages/1_👤_Player_Rating.py
@@ -3,8 +3,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-#import shap
-#from streamlit_shap import st_shap
+import shap
+from streamlit_shap import st_shap
 import pickle
 import plotly.graph_objects as go
 fig = go.Figure()
